aaa3f310e1ee856cef62042602c71e116827d37a
osddeitf <osddeitf@gmail.com>
2020-04-23 22:22:46 +0700

    Add webhook feature

--- src/main/java/com/osddeitf/antmedia/MinioStorageClient.java
+++ src/main/java/com/osddeitf/antmedia/MinioStorageClient.java
@@ -2,8 +2,10 @@ package com.osddeitf.antmedia;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
@@ -15,21 +17,32 @@ import io.minio.PutObjectOptions;
 import io.minio.errors.ErrorResponseException;
 import io.minio.errors.InvalidEndpointException;
 import io.minio.errors.InvalidPortException;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MinioStorageClient extends StorageClient {
 
     private MinioClient minioClient;
     private String url;
+    private String webhookURL = null;
 
-	protected static Logger logger = Logger.getLogger(MinioStorageClient.class.getName());
+    protected OkHttpClient httpClient;
+    protected static Logger logger = Logger.getLogger(MinioStorageClient.class.getName());
+    private static final String ACTION_NAME = "vodUploaded";
+
+    public MinioStorageClient() {
+        this.httpClient = new OkHttpClient.Builder()
+            // .connectTimeout(timeout, unit)
+            .callTimeout(10, TimeUnit.SECONDS)
+            .build();
+    }
 
     private MinioClient getMinioClient() throws InvalidEndpointException, InvalidPortException {
         if (minioClient == null) {
-            minioClient = new MinioClient(
-                this.getUrl(),
-                this.getAccessKey(),
-                this.getSecretKey()
-            );
+            minioClient = new MinioClient(this.getUrl(), this.getAccessKey(), this.getSecretKey());
         }
         return minioClient;
     }
@@ -42,6 +55,58 @@ public class MinioStorageClient extends StorageClient {
         this.url = url;
     }
 
+    public String getWebhookURL() {
+        return webhookURL;
+    }
+
+    public void setWebhookURL(String url) {
+        this.webhookURL = url;
+    }
+
+    /** Log utilities */
+    private void info(String message, Object ...args) {
+        logger.info(String.format(message, args));
+    }
+
+    private void error(String message, Object ...args) {
+        logger.log(java.util.logging.Level.SEVERE, String.format(message, args));
+    }
+
+    /** Webhook notify */
+    private void notifyWebhook(String filename) {
+        if (webhookURL == null || webhookURL.isEmpty()) return;
+        
+        info("Webhook `%s` invoking at %s with parameters: %s", ACTION_NAME, webhookURL, filename);
+        
+        RequestBody body = new FormBody.Builder()
+            // .addEncoded("id", "")
+            // .addEncoded("vodId", "")
+            .addEncoded("action", ACTION_NAME)
+            .addEncoded("vodName", filename)
+            .build();
+
+        Request request = new Request.Builder()
+            .url(webhookURL)
+            // .header("Content-Type", "application/x-www-form-urlencoded")
+            .post(body)
+            .build();
+        
+        try {
+            Response response = httpClient.newCall(request).execute();
+            if (response.isSuccessful()) {
+                info("Webhook `%s` %s success", ACTION_NAME, filename);
+            }
+            else {
+                error("Webhook `%s` %s failed with status code %d", ACTION_NAME, filename, response.code());
+            }
+            response.close();
+        }
+        catch (IOException exception) {
+            error("Webhook `%s` %s failed - Network error", ACTION_NAME, filename);
+        }
+    }
+
+    /** Interface declarations */
     @Override
     public boolean fileExist(String filename, FileType type) {
         try {
@@ -51,10 +116,10 @@ public class MinioStorageClient extends StorageClient {
         catch (ErrorResponseException e) {
             if (e.errorResponse().errorCode() == ErrorCode.NO_SUCH_OBJECT)
                 return false;
-            logger.log(java.util.logging.Level.SEVERE, e.toString());
+            error(e.toString());
         }
         catch (Exception e) {
-            logger.log(java.util.logging.Level.SEVERE, ExceptionUtils.getStackTrace(e));
+            error(ExceptionUtils.getStackTrace(e));
         }
         return false;
     }
@@ -65,14 +130,14 @@ public class MinioStorageClient extends StorageClient {
             getMinioClient().removeObject(getStorageName(), filename);
         }
         catch (Exception e) {
-            logger.log(java.util.logging.Level.SEVERE, ExceptionUtils.getStackTrace(e));
+            error(ExceptionUtils.getStackTrace(e));
         }
     }
 
     @Override
     public void save(File file, FileType type) {
         try {
-            logger.info("Mp4 `" + file.getName() + "` upload has started");
+            info("Mp4 `%s` upload to MinIO has started", file.getName());
 
             InputStream inputStream = new FileInputStream(file);
             getMinioClient().putObject(
@@ -83,10 +148,12 @@ public class MinioStorageClient extends StorageClient {
             );
             inputStream.close();
             Files.delete(file.toPath());
-            logger.info("File `" + file.getName() + "` uploaded to MinIO");
+            notifyWebhook(file.getName());
+
+            info("File `%s` uploaded to MinIO", file.getName());
         }
         catch (Exception e) {
-            logger.log(java.util.logging.Level.SEVERE, ExceptionUtils.getStackTrace(e));
+            error(ExceptionUtils.getStackTrace(e));
         }
     }
 }
